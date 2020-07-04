fdd1aaf49f146d60c6ceca6248c97c47d00c825e
Nina Tymińska <nina.tyminska@k2.pl>
2020-07-04 23:32:23 +0200

    fix(404): not found page after refactor

--- src/js/router.js
+++ src/js/router.js
@@ -2,18 +2,16 @@ import Router from 'vanilla-router';
 import { errorTemplate } from './hdbTemplates';
 import { isUserAuthenticated } from './user';
 
-window.addEventListener('load', () => {
-    const el = $('.main__content--app');
+const el = $('.main__content--app');
 
-    export const router = new Router({
-        mode: 'history',
-        page404: async () => {
-            await isUserAuthenticated();
+export const router = new Router({
+    mode: 'history',
+    page404: async () => {
+        await isUserAuthenticated();
 
-            const html = errorTemplate();
-            el.html(html);
+        const html = errorTemplate();
+        el.html(html);
 
-            $('.main__content--loader').fadeOut();
-        },
-    });
+        $('.main__content--loader').fadeOut();
+    },
 });
